b5f932d728b8088ec5ba81386dfc77bdebdd13a1
Lucas Doyle <lucas.doyle@samsara.com>
2020-07-19 16:55:26 -0700

    misc

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,5 +4,5 @@ export const CACHE_FOLDER = path.resolve(".cache");
 export const CAPTURE_FOLDER = path.resolve("captures");
 export const THUMBS_FOLDER_NAME = ".thumbs";
 export const VIEWER_FOLDER = path.resolve("./ptz-viewer/build");
-export const SERVER_PORT = 4001;
+export const SERVER_PORT = process.env.SERVER_PORT || 4001;
 export const CONFIG_FILE = path.resolve("config.json");
