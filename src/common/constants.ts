ca870d136617541abcde820b4a792ce31a52041d
Lucas Doyle <lucas.doyle@samsara.com>
2020-07-20 19:38:10 -0700

    support BASE_PATH

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,8 +1,11 @@
 import * as path from "path";
 
-export const CACHE_FOLDER = path.resolve(".cache");
-export const CAPTURE_FOLDER = path.resolve("captures");
+const formatPath = (s: string) =>
+  process.env.BASE_PATH ? `${process.env.BASE_PATH}/${s}` : path.resolve(s);
+
+export const CACHE_FOLDER = formatPath(".cache");
+export const CAPTURE_FOLDER = formatPath("captures");
 export const THUMBS_FOLDER_NAME = ".thumbs";
-export const VIEWER_FOLDER = path.resolve("./ptz-viewer/build");
+export const VIEWER_FOLDER = formatPath("./ptz-viewer/build");
 export const SERVER_PORT = parseInt(process.env.SERVER_PORT, 10) || 4001;
-export const CONFIG_FILE = path.resolve("config.json");
+export const CONFIG_FILE = formatPath("config.json");
