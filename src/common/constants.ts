642406e4dc1e09bd87a6bae10d0bebbd36ce06d5
Lucas Doyle <lucas.doyle@samsara.com>
2020-07-20 20:04:30 -0700

    whatever

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,11 +1,14 @@
 import * as path from "path";
 
 const formatPath = (s: string) =>
-  process.env.BASE_PATH ? `${process.env.BASE_PATH}/${s}` : path.resolve(s);
+  process.env.BASE_PATH
+    ? `${(process as any).env.BASE_PATH}/${s}`
+    : path.resolve(s);
 
 export const CACHE_FOLDER = formatPath(".cache");
 export const CAPTURE_FOLDER = formatPath("captures");
 export const THUMBS_FOLDER_NAME = ".thumbs";
 export const VIEWER_FOLDER = formatPath("./ptz-viewer/build");
-export const SERVER_PORT = parseInt(process.env.SERVER_PORT, 10) || 4001;
+export const SERVER_PORT =
+  parseInt((process as any).env.SERVER_PORT, 10) || 4001;
 export const CONFIG_FILE = formatPath("config.json");
