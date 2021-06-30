57c2a90b72884c52ce66a213dcf723d1d03bb113
Lucas Doyle <lucas.doyle@samsara.com>
2021-06-29 20:41:36 -0700

    fix wss

--- timelapse-viewer/src/utils/api.ts
+++ timelapse-viewer/src/utils/api.ts
@@ -3,11 +3,15 @@ import { SERVER_PORT } from "../common/constants";
 import { MILLISECONDS_IN_SECOND } from "../common/time";
 import { encode } from "../common/encode";
 
+const isHttps = window.location.protocol.startsWith("https");
 export const HTTP_BASE_URL =
-  process.env.PUBLIC_URL || `http://${window.location.hostname}:${SERVER_PORT}`;
+  process.env.PUBLIC_URL ||
+  `${window.location.protocol}//${window.location.hostname}:${SERVER_PORT}`;
 export const WS_BASE_URL = process.env.PUBLIC_URL
-  ? `ws://${window.location.hostname}${process.env.PUBLIC_URL}`
-  : `ws://${window.location.hostname}:${SERVER_PORT}`;
+  ? `ws${isHttps ? "s" : ""}://${window.location.hostname}${
+      process.env.PUBLIC_URL
+    }`
+  : `ws${isHttps ? "s" : ""}://${window.location.hostname}:${SERVER_PORT}`;
 
 export const apiFetch = async (url: string): Promise<any> => {
   const res = await window.fetch(`${HTTP_BASE_URL}/${url}`);
